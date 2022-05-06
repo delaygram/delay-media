9d6fb23afdc138b5b29c992cb6571e7f7af8f50f
Rick Meels <rick.meels@gmail.com>
2022-05-06 15:22:09 +0200

    Added api spec

--- functions/upload/app.py
+++ functions/upload/app.py
@@ -1,9 +1,18 @@
 import boto3
 import botocore.exceptions
 import json
+from cgi import parse_header, parse_multipart
+from io import BytesIO
 
 
 def lambda_handler(event, context):
+
+    c_type, c_data = parse_header(event['headers']['Content-Type'])
+    assert c_type == 'multipart/form-data'
+    form_data = parse_multipart(BytesIO(event['body'].decode('base64')), c_data)
+
+    print(form_data)
+
     return {
         "statusCode": 200,
         "body": json.dumps({
